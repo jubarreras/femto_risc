4f871104f0de242f374a5a5de2a4e5bc4bac1cc7
Juan David Barrera Salamanca <jubarreras@unal.edu.co>
2025-11-04 20:46:52 -0500

    Update MappedSPIRAM.v

--- src/MappedSPIRAM.v
+++ src/MappedSPIRAM.v
@@ -1,13 +1,13 @@
- `define SPI_FLASH_DUMMY_CLOCKS 0
+  `define SPI_FLASH_DUMMY_CLOCKS 0
 
 module MappedSPIRAM( 
     input wire 	        clk,           // system clock
     input wire          reset,         // system reset
     input wire 	        rd,         // read strobe
     input wire          wr,         // write strobe		
-    input wire [19:0]   word_address,  // address of the word to be read
+	input wire [15:0]   word_address,  // address of the word to be read
 
-    input wire [31:0]   wdata,         // data to be written
+	input wire [31:0]   wdata,         // data to be written
     output wire [31:0]  rdata,         // data read
     output reg          rbusy,        // asserted if busy receiving data
     output reg          wbusy,         // asserted if busy writing data		    
@@ -20,27 +20,23 @@ module MappedSPIRAM(
 );
 
 
- parameter START      = 2'b00;
- parameter WAIT_INST  = 2'b01;
- parameter SEND       = 2'b10;
- parameter RECEIVE    = 2'b11;
+ parameter START      = 3'b000;
+ parameter WAIT_INST  = 3'b001;
+ parameter SEND       = 3'b010;
+ parameter RECEIVE    = 3'b011;
+ parameter WAIT_SCLK  = 3'b100;
 
- parameter divisor    = 2;
+ parameter divisor    = 10;
 
-
- 
- reg [1:0] state;
+ reg edge_CLK;
+ reg [2:0] state;
  reg clk_div;
 
-
-
-   reg [5:0]  snd_bitcount;
-   reg [31:0] cmd_addr;
+   reg [8:0]  snd_bitcount;
+   reg [63:0] cmd_addr;
    reg [5:0]  rcv_bitcount;
    reg [31:0] rcv_data;
-
    reg [5:0]  div_counter;
-
 
 always @(negedge clk) begin
     if (!reset) begin
@@ -66,6 +62,11 @@ always @(negedge clk) begin
     else begin
       if ( (div_counter == divisor/2) | ( div_counter == divisor )   ) begin
         CLK  <= ~CLK;
+        edge_CLK <= 1;
+      end
+      else begin
+        CLK <= CLK;
+        edge_CLK <= 0;
       end
     end
 end
@@ -83,61 +84,67 @@ always @(negedge clk) begin
       cmd_addr <= 0;
     end else begin
     case(state)
+
       START:begin
         CS_N         <= 1'b1;
         rbusy        <= 1'b0;
-        wbusy        <= 1'b0;
         snd_bitcount <= 6'd0;
         rcv_bitcount <= 6'd0;
         state        <= WAIT_INST;
+        wbusy        <= 1'b0;
       end
 
       WAIT_INST: begin
+        state        <= WAIT_INST;
         if (rd) begin
           CS_N         <= 1'b0;
           rbusy        <= 1'b1;
-          wbusy        <= 1'b0;
-          snd_bitcount <= 6'd24;
+          snd_bitcount <= 8'd32;
+          cmd_addr     <= {8'h03, 8'h00, word_address[15:0], 32'd0};
+          state        <= WAIT_SCLK;
           rcv_bitcount <= 6'd32;
-          cmd_addr     <= {8'h03,word_address[15:0],8'h00};
-          state        <= SEND;
+          wbusy        <= 1'b0;
         end
-        else if (wr) begin
+        if (wr) begin
           CS_N         <= 1'b0;
           rbusy        <= 1'b0;
           wbusy        <= 1'b1;
-          snd_bitcount <= 6'd32;
+          snd_bitcount <= 8'd64;
           rcv_bitcount <= 6'd0;
-          cmd_addr     <= {8'h02,word_address[15:0],wdata[7:0]};
-          state        <= SEND;
+          cmd_addr     <= {8'h02, 8'h00, word_address[15:0], wdata[31:0]};
+          state        <= WAIT_SCLK;
         end
-        else begin
-          state        <= WAIT_INST;
-        end
+        //else begin
+        //  state        <= WAIT_INST;
+        //end
+      end
+
+      WAIT_SCLK: begin
+        state <= SEND;
       end
 
       SEND: begin
-        if(CLK) begin
+        if(clk_div) begin
             if(snd_bitcount == 1) begin
                 state        <= RECEIVE;
             end
             else begin
             snd_bitcount <= snd_bitcount - 6'd1;
-            cmd_addr     <= {cmd_addr[30:0],1'b1};
+            cmd_addr     <= {cmd_addr[62:0],1'b1};
             state        <= SEND;
             end
         end
       end
 
       RECEIVE: begin
-        if(CLK) begin
-          if(rcv_bitcount == 0) begin
+        if(clk_div) begin
+          if(rcv_bitcount <= 1) begin
             state         <= START;
           end
           else begin
             rcv_bitcount <= rcv_bitcount - 6'd1;
             rcv_data     <= {rcv_data[30:0],MISO};
-          state         <= RECEIVE;  
+          state         <= RECEIVE;
           end
         end
       end
@@ -148,15 +155,12 @@ always @(negedge clk) begin
     endcase
   end
 end
-
-
-   assign  MOSI  = cmd_addr[31];
+   assign  MOSI  = cmd_addr[63];
 
 //   assign  CLK   = !CS_N && !clk; // CLK needs to be inverted (sample on posedge, shift of negedge) 
                                   // and needs to be disabled when not sending/receiving (&& !CS_N).
 
    // since least significant bytes are read first, we need to swizzle...
-   assign rdata = {rcv_data[7:0],rcv_data[15:8],rcv_data[23:16],rcv_data[31:24]};
-
+   assign rdata = rcv_data;
 
 endmodule
